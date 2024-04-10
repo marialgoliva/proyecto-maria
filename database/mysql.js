50aadf2b9015ad25647609bda80229abd6017c43
marialgoliva <146101143+marialgoliva@users.noreply.github.com>
2024-04-10 20:58:05 +0200

    fix mysql file

--- database/mysql.js
+++ database/mysql.js
@@ -1,11 +1,11 @@
-import mysql from 'serverless-mysql'
+import mysql from "serverless-mysql";
 
 export const conn = mysql({
-    config:{
-        host: process.env.MYSQL_HOST,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        port: process.env.MYSQL_DATABASE,
-        database: process.env.MYSQL_PORT
-    }
-})
+  config: {
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    port: process.env.MYSQL_PORT,
+    database: process.env.MYSQL_DATABASE,
+  },
+});
