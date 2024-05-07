30e3710487e3f969492ed828b0f813e15d74549a
marialgoliva <146101143+marialgoliva@users.noreply.github.com>
2024-05-07 22:40:19 +0200

    edited crea_bd

--- database/crea_bd.sql
+++ database/crea_bd.sql
@@ -5,15 +5,15 @@ CREATE DATABASE IF NOT EXISTS PROYECTO_MARIA;
 USE PROYECTO_TEST;
 
 /*Crear las tablas*/
-CREATE TABLE USUARIO (
-    dni CHAR(9) PRIMARY KEY,
-    nombre VARCHAR(30) NOT NULL,
-    apellidos VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    username VARCHAR(25) NOT NULL,
-    password CHAR(60) NOT NULL,
-    rol ENUM('user','admin') DEFAULT 'user' NOT NULL
-);
+-- CREATE TABLE USUARIO (
+--     dni CHAR(9) PRIMARY KEY,
+--     nombre VARCHAR(30) NOT NULL,
+--     apellidos VARCHAR(50) NOT NULL,
+--     email VARCHAR(100) NOT NULL UNIQUE,
+--     username VARCHAR(25) NOT NULL,
+--     password CHAR(60) NOT NULL,
+--     rol ENUM('user','admin') DEFAULT 'user' NOT NULL
+-- );
 
 CREATE TABLE USUARIOS (
     dni CHAR(9) PRIMARY KEY,
@@ -25,44 +25,47 @@ CREATE TABLE USUARIOS (
     rol ENUM('user','admin') DEFAULT 'user' NOT NULL
 );
 
-CREATE TABLE CLIENTE (
-    dni CHAR(9) PRIMARY KEY,
-    calle VARCHAR(100),
-    ciudad VARCHAR(50) NOT NULL,
-    cp CHAR(5) NOT NULL,
-    FOREIGN KEY (dni) REFERENCES Usuario(dni)
-    );
+-- CREATE TABLE CLIENTE (
+--     dni CHAR(9) PRIMARY KEY,
+--     calle VARCHAR(100),
+--     ciudad VARCHAR(50) NOT NULL,
+--     cp CHAR(5) NOT NULL,
+--     FOREIGN KEY (dni) REFERENCES Usuario(dni)
+--     );
 
 CREATE TABLE CLIENTES (
     email VARCHAR(100) PRIMARY KEY,
-    calle VARCHAR(100),
+    calle VARCHAR(100) NOT NULL,
     ciudad VARCHAR(50) NOT NULL,
-    cp CHAR(5) NOT NULL
+    cp CHAR(5) NOT NULL,
+    nombre VARCHAR(250),
+    dni CHAR(9)
     );
     
 CREATE TABLE PEDIDOS (
 	idPedido INT AUTO_INCREMENT PRIMARY KEY,
-    idCliente VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
     fechaPedido DATE NOT NULL,
     fechaEntrega DATE,
     estado VARCHAR(100),
     tipoPago ENUM('efectivo','bizum','tarjeta'),
     importeTotal DOUBLE NOT NULL,
-    FOREIGN KEY (idCliente) REFERENCES Clientes(email)
+    idCliente CHAR(9),
+    FOREIGN KEY (email) REFERENCES CLIENTES(email)
     
 );
 
-CREATE TABLE PEDIDO (
-	idPedido INT AUTO_INCREMENT PRIMARY KEY,
-    idCliente CHAR(9) NOT NULL,
-    fechaPedido DATE NOT NULL,
-    fechaEntrega DATE,
-    estado VARCHAR(100),
-    tipoPago ENUM('mano','bizum','transferencia'),
-    importeTotal DOUBLE NOT NULL,
-    FOREIGN KEY (idCliente) REFERENCES Cliente(dni)
+-- CREATE TABLE PEDIDO (
+-- 	idPedido INT AUTO_INCREMENT PRIMARY KEY,
+--     idCliente CHAR(9) NOT NULL,
+--     fechaPedido DATE NOT NULL,
+--     fechaEntrega DATE,
+--     estado VARCHAR(100),
+--     tipoPago ENUM('mano','bizum','transferencia'),
+--     importeTotal DOUBLE NOT NULL,
+--     FOREIGN KEY (idCliente) REFERENCES Cliente(dni)
     
-);
+-- );
 
 CREATE TABLE CATEGORIA (
 	idCategoria INT AUTO_INCREMENT PRIMARY KEY,
@@ -87,8 +90,8 @@ CREATE TABLE PEDIDO_PRODUCTO(
     id INT AUTO_INCREMENT PRIMARY KEY,
     idPedido INT NOT NULL,
     idProducto INT NOT NULL,
-    precioUnitario DOUBLE,
-    cantidad INTEGER,
+    cantidad INT,
+    talla VARCHAR(4),
     FOREIGN key (idPedido) REFERENCES Pedidos(idPedido),
     FOREIGN KEY (idProducto) REFERENCES Producto(idProducto),
 );
@@ -97,7 +100,7 @@ CREATE TABLE COMENTARIO (
   	idComentario  INT AUTO_INCREMENT PRIMARY KEY,
     idCliente CHAR(9),
     idProducto INT NOT NULL,
-    puntuacion INTEGER NOT NULL,
+    puntuacion INT NOT NULL,
     texto TEXT,
     FOREIGN KEY (idCliente) REFERENCES Cliente(dni),
     FOREIGN KEY (idProducto) REFERENCES Producto(idProducto)
@@ -105,8 +108,8 @@ CREATE TABLE COMENTARIO (
 
 CREATE TABLE STOCK (
   	idProducto INT ,
-    talla VARCHAR(25),
-    stock INTEGER NOT NULL,
+    talla VARCHAR(25) NOT NULL,
+    stock INT NOT NULL,
     PRIMARY KEY (idProducto,talla),
     FOREIGN KEY (idProducto) REFERENCES Producto(idProducto)
 );
