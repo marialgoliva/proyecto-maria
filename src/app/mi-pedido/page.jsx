7b95ffab300b10d4b3e92d9ac49ccfa549fc7bd1
marialgoliva <146101143+marialgoliva@users.noreply.github.com>
2024-05-07 21:58:40 +0200

    fix error user without order

--- src/app/mi-pedido/page.jsx
+++ src/app/mi-pedido/page.jsx
@@ -8,37 +8,46 @@ import PedidoCliente from "@/components/card/PedidoCliente";
 async function OrderPage() {
   const session = await getServerSession(authOptions);
   const idsPedidos = await getPedidoCliente(session.user.email);
+  if (idsPedidos) {
+    const pedidos = await Promise.all(
+      idsPedidos.map(async (data) => {
+        const productosPedido = await getProductosPedido(data.idPedido);
+        return productosPedido
+          ? {
+              id: data.idPedido,
+              infoPedido: productosPedido,
+            }
+          : null;
+      }),
+    );
+    const pedidosFiltrados = pedidos.filter((pedido) => pedido !== null);
 
-  const pedidos = await Promise.all(
-    idsPedidos.map(async (data) => {
-      const productosPedido = await getProductosPedido(data.idPedido);
-      return productosPedido
-        ? {
-            id: data.idPedido,
-            infoPedido: productosPedido,
-          }
-        : null;
-    }),
-  );
-  const pedidosFiltrados = pedidos.filter((pedido) => pedido !== null);
-
-  pedidosFiltrados.forEach;
+    pedidosFiltrados.forEach;
+  }
 
   return (
     <div className="d-flex flex-column m-4">
-      <h2 className="title">
-        Hola {session.user.name}, este es el resumen de tus pedidos:
-      </h2>
-      <div>
-        {pedidosFiltrados.map((pedido, index) => (
-          <div key={index} className="m-5">
-            <h4>#{pedido.id}</h4>
-            <div className="w-50 bg-light p-3 rounded shadow-sm ">
-              <PedidoCliente dataProductos={pedido.infoPedido} />
-            </div>
+      {idsPedidos ? (
+        <>
+          <h2 className="title">
+            {session.user.name}, este es el resumen de tus pedidos:
+          </h2>
+          <div>
+            {pedidosFiltrados.map((pedido, index) => (
+              <div key={index} className="m-5">
+                <h4>#{pedido.id}</h4>
+                <div className="w-50 bg-light p-3 rounded shadow-sm ">
+                  <PedidoCliente dataProductos={pedido.infoPedido} />
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </>
+      ) : (
+        <h2 className="title">
+          {session.user.name}, no tienes ningún pedido guardado.
+        </h2>
+      )}
     </div>
   );
 }
