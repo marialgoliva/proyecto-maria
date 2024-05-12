37f125ccb2094842b699f169e640e9d7e2bdf9f4
marialgoliva <146101143+marialgoliva@users.noreply.github.com>
2024-05-12 17:52:16 +0200

    change .env and not minimiser

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,21 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   async redirects() {
-//     return [
-//       {
-//         source: "/admin",
-//         destination: "/admin",
-//         permanent: true,
-//       },
-//     ];
-//   },
-// };
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/admin",
+        destination: "/admin",
+        permanent: true,
+      },
+    ];
+  },
+  webpack: (config, { dev, isServer }) => {
+    // deshabilitar la minimización en producción
+    if (!dev && !isServer) {
+      config.optimization.minimize = false;
+    }
+    return config;
+  },
+};
 
-// export default nextConfig;
+export default nextConfig;
